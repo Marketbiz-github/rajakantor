5ee5327cc67f25cd51e001288ed152c33956052e
Cyrilus Santio Pranata <yeye@marketbiz.net>
2026-06-29 09:58:08 +0700

    Update index and remove slider5.php

--- public/index.php
+++ public/index.php
@@ -1,20 +1,21 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register the Composer autoloader...
+// 1. Muat Autoloader Composer
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
+// 2. Muat Bootstrap App & Tangani Permintaan
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+// 3. Kirim Respon
+$response->send();
+
+// 4. Hentikan Kernel
+$kernel->terminate($request, $response);
